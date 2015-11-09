1cde7c4d18a020d9d0f62802e3a0ba095643f847
marmch <mark3612@gmail.com>
2015-11-08 19:03:45 -0500

    Added output throttle

--- drone_receiver/drone_receiver.ino
+++ drone_receiver/drone_receiver.ino
@@ -1,27 +1,33 @@
 int throttlePin = 8;
+int throttleOut = 9;
 int throttleHighCount = 0;
 int throttleCount = 0;
 int throttle = 0;
 int temp = 0;
-
-void writePWM(double percentWidth, int pin){
-  Serial.println(percentWidth);
-}
+double percent = 0;
+bool throttleHigh = false;
+unsigned long pulseStart = 0;
+unsigned long pulseLength = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(throttlePin, INPUT);
+  pinMode(throttleOut, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+  unsigned long newTime = micros();
+  
+  //Read Input
   int newThrottle = digitalRead(throttlePin);
   if(newThrottle == HIGH && throttle == LOW){
-    double percent = (double)throttleHighCount/throttleCount;
-    //writePWM(percent, throttlePin);
-    temp++;
-    if(temp%10 == 0)
-      Serial.println(percent*100, 4);
+    percent = (double)throttleHighCount/throttleCount;
+    pulseLength = newTime - pulseStart;
+    pulseStart = micros();
+    //temp++;
+    //if(temp%10 == 0)
+    //  Serial.println(percent*100, 4);
     throttleCount = 0;
     throttleHighCount = 0;
   }
@@ -29,5 +35,16 @@ void loop() {
     throttleHighCount++;
   throttleCount++;
   throttle = newThrottle;
-  delayMicroseconds(5);
+  
+  //Write Output
+  unsigned long pulseTime = newTime - pulseStart;
+  if(pulseTime == 0 && !throttleHigh){
+    digitalWrite(throttleOut, HIGH);
+    throttleHigh = true;
+  }
+  if(pulseTime > pulseLength * percent && throttleHigh){
+    digitalWrite(throttleOut, LOW);
+    throttleHigh = false;
+  }
+  delayMicroseconds(2);
 }
